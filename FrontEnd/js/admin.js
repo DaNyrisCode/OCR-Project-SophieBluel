1717788ed19b4ac9511e958d3cabdd01fc75f94b
DaNyrisCode <komakhinamarina@gmail.com>
2024-10-11 20:28:47 +0200

    modal v2

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -2,23 +2,19 @@ import { getCategoriesFromApi, getWorksFromApi } from "./api.js";
 // Le fouteur de trouble
 // import { displayMyProjects } from "./app.js"; 
 
-(async () => {
-    const allWorks = await getWorksFromApi();
-    const categories = await getCategoriesFromApi();
+const allWorks = await getWorksFromApi();
+const categories = await getCategoriesFromApi();
 
-    const modifyBtn = document.querySelector('.edit-app span');
-    const modal = document.querySelector('#modal1');
+const modifyBtn = document.querySelector('.edit-app span');
+const modal = document.querySelector('#modal1');
 
-    if (modifyBtn) {
-        modifyBtn.addEventListener('click', () => {
-            modal.classList.add('active');
-            
-        });
+modifyBtn.addEventListener('click', () => {
+    modal.classList.add('active');
+});
+
+document.querySelector('.modal').addEventListener('click', (e) => {
+    if (!e.target.closest('.modal__content')) {
+        modal.classList.remove('active');
     }
-
-    document.querySelector('.modal').addEventListener('click', (e) => {
-        if (!e.target.closest('.modal__content')) {
-            modal.classList.remove('active');
-        }
-    });
-})();
+});
+;
