5dbc29eda8657efb86fa394c49886ebab57a36c4
DaNyrisCode <komakhinamarina@gmail.com>
2024-10-14 17:27:19 +0200

    Ajout permute modale1 => modale2

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -9,32 +9,49 @@ displayMyProjects(allWorks, mainGallery);
 
 // Gestion modale
 const modifyBtn = document.querySelector('.edit-app span');
-const modal = document.querySelector('#modal1');
-const closeModalBtn = document.querySelector('.close-btn');
+const modal1 = document.querySelector('#modal1');
+const modal2 = document.querySelector('#modal2');
+const closeModalBtns = document.querySelectorAll('.close-btn');
+const addPhotoBtn = document.querySelector('.add-photo-btn');
 
+// Fermeture - Ouverture Modale
 const closeModal = (modal) => {
     modal.classList.remove('active');
 };
+const openModal = (modal) => {
+    modal.classList.add('active');
+};
 
-    // Ouverture Modale
-    if (modifyBtn) {
-        modifyBtn.addEventListener('click', () => {
-            modal.classList.add('active');
+// Ouverture Modale depuis le Btn modifier
+if (modifyBtn) {
+    modifyBtn.addEventListener('click', () => {
+        openModal(modal1);
 
-            const modalGallery = document.querySelector('.modal__gallery');displayMyProjects(allWorks, modalGallery);
-        });
-    }
+        const modalGallery = document.querySelector('.modal__gallery');displayMyProjects(allWorks, modalGallery);
+    });
+}
 
-    // Fermeture via Btn
-    if (closeModalBtn) {
-        closeModalBtn.addEventListener('click', () => {
-            closeModal(modal);
-        });
-    }
+// Fermeture via Btn X
+closeModalBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const modal = btn.closest('.modal');
+        closeModal(modal);
+    });
+});
 
-    // Fermeture via click en dehors
-    document.querySelector('.modal').addEventListener('click', (e) => {
+// Fermeture via click en dehors
+document.querySelectorAll('.modal').forEach(modal => {
+    modal.addEventListener('click', (e) => {
         if (!e.target.closest('.modal__content')) {
             closeModal(modal);
         }
     });
+});
+
+// Permut modale 1 => modale 2
+if (addPhotoBtn) {
+    addPhotoBtn.addEventListener('click', () => {
+        closeModal(modal1);
+        openModal(modal2);
+    });
+}
