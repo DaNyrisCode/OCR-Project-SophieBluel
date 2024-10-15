49ce1970d70a949fec967249f2f030e83de55c22
DaNyrisCode <komakhinamarina@gmail.com>
2024-10-15 12:08:38 +0200

    Mep des switch entre les modales

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -5,53 +5,61 @@ const allWorks = await getWorksFromApi();
 const categories = await getCategoriesFromApi();
 
 const mainGallery = document.querySelector('.gallery');
+
+// Injection HTML des Projets
 displayMyProjects(allWorks, mainGallery);
+
 
 // Gestion modale
 const modifyBtn = document.querySelector('.edit-app span');
 const modal1 = document.querySelector('#modal1');
 const modal2 = document.querySelector('#modal2');
 const closeModalBtns = document.querySelectorAll('.close-btn');
+const prevBtn = document.querySelector('.prev-btn');
 const addPhotoBtn = document.querySelector('.add-photo-btn');
 
-// Fermeture - Ouverture Modale
-const closeModal = (modal) => {
-    modal.classList.remove('active');
+
+// Ouverture Modale - Fermeture Modale
+const toggleModal = (modal, show) => {
+    modal.classList.toggle('active', show);
 };
-const openModal = (modal) => {
-    modal.classList.add('active');
+
+// Switch entre 2 Modales
+const switchModal = (currentModal, nextModal) => {
+    currentModal.classList.remove('active');
+    nextModal.classList.add('active');
 };
+
 
 // Ouverture Modale depuis le Btn modifier
-if (modifyBtn) {
-    modifyBtn.addEventListener('click', () => {
-        openModal(modal1);
+modifyBtn.addEventListener('click', () => {
+    toggleModal(modal1, true);
 
-        const modalGallery = document.querySelector('.modal__gallery');displayMyProjects(allWorks, modalGallery);
-    });
-}
+    const modalGallery = modal1.querySelector('.modal__gallery');
+    displayMyProjects(allWorks, modalGallery);
+});
 
 // Fermeture via Btn X
 closeModalBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const modal = btn.closest('.modal');
-        closeModal(modal);
+        toggleModal(modal, false);
     });
 });
 
 // Fermeture via click en dehors
 document.querySelectorAll('.modal').forEach(modal => {
     modal.addEventListener('click', (e) => {
-        if (!e.target.closest('.modal__content')) {
-            closeModal(modal);
-        }
+        if (!e.target.closest('.modal__content')) toggleModal(modal, false);
     });
 });
 
 // Permut modale 1 => modale 2
-if (addPhotoBtn) {
-    addPhotoBtn.addEventListener('click', () => {
-        closeModal(modal1);
-        openModal(modal2);
-    });
-}
+addPhotoBtn.addEventListener('click', () => {
+    switchModal(modal1, modal2);
+});
+
+// Permut modale 2 => modale 1
+prevBtn.addEventListener('click', () => {
+    switchModal(modal2, modal1);
+})
